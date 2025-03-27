4339746f63899afb840c039ed886d0581aa367b3
予纾 <yushu@srcz.one>
2025-03-27 11:38:46 +0800

    fix: fix file server options

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,19 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.FileProviders;
 
-builder.Environment.WebRootPath = builder.Environment.ContentRootPath;
+WebApplicationBuilder builder = WebApplication.CreateSlimBuilder(args);
 
 WebApplication app = builder.Build();
 
-app.UseFileServer();
+FileServerOptions fileServerOptions = new()
+{
+    FileProvider = new PhysicalFileProvider(builder.Environment.ContentRootPath)
+    {
+        UseActivePolling = true,
+        UsePollingFileWatcher = true
+    }
+};
+fileServerOptions.StaticFileOptions.ServeUnknownFileTypes = true;
+
+app.UseFileServer(fileServerOptions);
 
 app.Run();
